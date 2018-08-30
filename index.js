7e6a42c957f323df3d36b1b835285325cef3de2b
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-30 11:59:46 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const reverse = require("esrever");
+
+/**
+	* Checks if a string is palindromic.
+	* @param {string} string The string to check.
+	* @param {boolean} caseSensitive Whether to consider different casing as not palindromic.
+	* @returns {boolean}
+*/
+module.exports = (string, caseSensitive = true) => {
+	const casedStr = caseSensitive ? string : string.toLowerCase();
+	return casedStr === reverse(casedStr);
+};
