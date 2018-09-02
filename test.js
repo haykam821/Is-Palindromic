b22884fcdc50e9ad0f9e788bc5c9c0f28cb09413
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-02 11:58:54 -0400

    Add extra test for returning boolean

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const ava = require("ava");
 const isPalindromic = require(".");
+
+const palin = "racecar";
 const casedPalin = "abcCBA";
+
+ava("returns boolean", function (test) {
+	test.is(typeof isPalindromic(palin), "boolean");
+});
 
 ava("case-sensitive option does not accept cased palindrome when unspecified", function (test) {
 	test.is(isPalindromic(casedPalin, true), false);
